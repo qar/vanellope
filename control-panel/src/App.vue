77f7e21e37ead57973a86451835e15f277f4f688
Qiao Anran <qiaoanran@gmail.com>
2017-11-27 18:06:42 +0800

    setup basic panel layout

--- control-panel/src/App.vue
+++ control-panel/src/App.vue
@@ -1,23 +1,75 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
 
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        min-height: 200px;
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+    .layout-menu-left{
+        background: #464c5b;
+    }
+    .layout-header{
+        height: 60px;
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+    .layout-logo-left{
+        width: 90%;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 15px auto;
+    }
+</style>
+<template>
+    <div class="layout">
+        <Row type="flex">
+            <Col span="4" class="layout-menu-left">
+                <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+                    <div class="layout-logo-left"></div>
+                    <Submenu name="1">
+                        <template slot="title">
+                            <Icon type="ios-navigate"></Icon>
+                            文章
+                        </template>
+                        <MenuItem name="1-1">已发布</MenuItem>
+                        <MenuItem name="1-2">草稿</MenuItem>
+                        <MenuItem name="1-3">回收站</MenuItem>
+                    </Submenu>
+                </Menu>
+            </Col>
+            <Col span="20">
+                <div class="layout-header"></div>
+                <div class="layout-content">
+                    <div class="layout-content-main">Content</div>
+                </div>
+                <div class="layout-copy">
+                    2011-2016 &copy; TalkingData
+                </div>
+            </Col>
+        </Row>
+    </div>
+</template>
 <script>
-export default {
-  name: 'app',
-};
+    export default {
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
