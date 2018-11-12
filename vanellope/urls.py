322990e1332da3fa05f1969ab6eff7f56ddd58e1
Qiao Anran <qiaoanran@gmail.com>
2018-11-13 00:59:11 +0800

    add delete url to delete friendlink

--- vanellope/urls.py
+++ vanellope/urls.py
@@ -49,6 +49,7 @@ routers = [
     (r"/api/v1/admin/trash/(.*)", apiv1.AdminTrashHandler),
     (r"/api/v1/comments", apiv1.CommentsHandler),
     (r"/api/v1/admin/friend-links", admin.FriendLinkHandler),
+    (r"/api/v1/admin/friend-links/(.*)", admin.FriendLinkHandler),
 
     # Feeds
     (r"/index.xml", feeds.MainFeed),
